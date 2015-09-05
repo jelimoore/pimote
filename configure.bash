088fe2b16b8363fc3179b2ce6e8e4926574df9b8
jelimoore <jack@drtecknow.com>
2015-09-04 20:47:23 -0500

    Update configure.bash

--- configure.bash
+++ configure.bash
@@ -17,6 +17,7 @@ echo "lirc_dev" >> /etc/modules
 echo "lirc_rpi gpio_in_pin=18 gpio_out_pin=22" >> /etc/modules
 echo "dtoverlay=lirc-rpi,gpio_in_pin=18,gpio_out_pin=22" >> /boot/config.txt
 
+#asking for overclock
 read -r -p "Would you like to overclock now? Please don't if you already have. (y/n) " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
@@ -30,26 +31,32 @@ else
     echo "Not overclocking."
 fi
 
+#Install LigHTTPD, PHP and enable PHP 
 apt-get install lighttpd -y
 apt-get install php5-common php5-cgi php5 -y
 lighty-enable-mod fastcgi-php
 sudo service lighttpd force-reload
 
+#Permissions for /var/www and Pi so Pi can write to /var/www without changing user
 chown www-data:www-data /var/www
 chmod 775 /var/www
 usermod -a -G www-data pi
 
+#eth0 ip
 echo "My IP for eth0 is:"
 ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
 
+#wlan0 ip
 echo "My IP for wlan0 is:"
 ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
 
+#copying CSS, JS and fonts from Bootstrap
 cp -R www/css /var/www/css
 cp -R www/js /var/www/js
 cp -R www/fonts /var/www/fonts
 rm /var/www/index.lighttpd.html
 
+#ask to reboot
 read -r -p "Would you like to reboot now? (y/n) " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
